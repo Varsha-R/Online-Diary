33ccbc957523b49f76c9466cc81b12813b00c857
Varsha Ravindra <varsharavindra12@gmail.com>
2018-11-26 10:40:56 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "", "onlinediary");
+		
+		if(mysqli_connect_error()) {
+			
+			die("Database connection error");
+		}
+
+?>
---
+++ header.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <style type="text/css">
+	  
+		.container {
+		
+			text-align: center;
+			width: 400px;
+			
+		}  
+		#homePageContainer {
+			
+			margin-top: 100px;
+		}
+		#containerLoggedInPage {
+			
+			margin-top: 30px;
+		}
+		html {
+			
+			background: url(background-image.jpg) no-repeat center center fixed;
+			background-size: cover;
+		}
+		body {
+			
+			background: none;
+			color: #FFF;
+		}
+		#loginform {
+			
+			display: none;
+		}
+		.toggleForms {
+			
+			font-weight: bold;
+		}
+		#diary {
+			
+			width: 100%;
+			height: 100%;
+			background-color: darkgray;
+			font-weight: bold;
+		}
+		#NavBar {
+			
+			font-weight: bolder;
+		}
+		
+	</style>
+  </head>
+  <body>
